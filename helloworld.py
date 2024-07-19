903f8fc0c702439b851a657cd033d21bcd3fe04c
vishal-2612 <github9990@gmail.com>
2024-07-19 10:34:54 +0530

    Add files via upload

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 27 13:22:07 2024
+
+@author: MSCS LAB-25
+"""
+
+def hello():
+    """print "hello world" and return none."""
+    print("hello world")
+
+hello()
+   
